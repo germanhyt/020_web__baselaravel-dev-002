cc34cf5770a53f070d951022f0b094c71cec1569
germanhytsystem <germanhuaytalla22@gmail.com>
2024-06-29 12:25:44 -0500

    feat: use of pattern repository and swagger UI for apirest students

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        // En caso de que no se usen recursos de la base de datos, se puede comentar la siguiente línea
+        $this->app->register(\L5Swagger\L5SwaggerServiceProvider::class);
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
